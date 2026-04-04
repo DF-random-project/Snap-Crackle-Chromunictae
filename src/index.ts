1fc882fdda441598a357683ea2828adacd364be1
whynotjava <tinycreeperx@gmail.com>
2026-04-03 22:27:16 -0400

    put in try catch

--- src/index.ts
+++ src/index.ts
@@ -24,57 +24,35 @@ export default {
 			console.log("/est 'lazy' listner got ran!");
 		});
 
-		app.command("/view",async ({context, body, client})=>{
-    // Call views.open with the built-in client
-    const result = await client.views.open({
-      // Pass a valid trigger_id within 3 seconds of receiving it
-      trigger_id: body.trigger_id,
-      // View payload
-      view: {
-        type: 'modal',
-        // View identifier
-        callback_id: 'view_1',
-        title: {
-          type: 'plain_text',
-          text: 'Modal title'
-        },
-        blocks: [
-          {
-            type: 'section',
-            text: {
-              type: 'mrkdwn',
-              text: 'Welcome to a modal with _blocks_'
-            },
-            accessory: {
-              type: 'button',
-              text: {
-                type: 'plain_text',
-                text: 'Click me!'
-              },
-              action_id: 'button_abc'
-            }
-          },
-          {
-            type: 'input',
-            block_id: 'input_c',
-            label: {
-              type: 'plain_text',
-              text: 'What are your hopes and dreams?'
-            },
-            element: {
-              type: 'plain_text_input',
-              action_id: 'dreamy_input',
-              multiline: true
-            }
-          }
-        ],
-        submit: {
-          type: 'plain_text',
-          text: 'Submit'
-        }
-      }
-    });
-    
+		app.command("/view", async ({ context, body, client }) => {
+		  try {
+		    const result = await client.views.open({
+		      trigger_id: body.trigger_id,
+		      view: {
+		        type: "modal",
+		        title: {
+		          type: "plain_text",
+		          text: "My Modal",
+		        },
+		        blocks: [
+		          {
+		            type: "section",
+		            text: {
+		              type: "mrkdwn",
+		              text: "This is a modal",
+		            },
+		          },
+		        ],
+		        submit: {
+		          type: "plain_text",
+		          text: "Submit",
+		        },
+		      },
+		    });
+		    console.log(result);
+		  } catch (error) {
+		    console.error(error);
+		  }
 		});
 
 		return await app.run(request, ctx);
